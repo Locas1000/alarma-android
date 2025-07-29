23f2ca818c1ca871e372ef73076929b3cfd27d4e
Lucas <lucascastineiras100@gmail.com>
2025-07-29 16:17:44 -0600

    Feat: Handle FAB on click event on main screen Chore: Ensure all files end with a newline

--- app/src/main/java/com/example/alarmapp/MainActivity.java
+++ app/src/main/java/com/example/alarmapp/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.alarmapp;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private FloatingActionButton addAlarmFab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +25,9 @@ public class MainActivity extends AppCompatActivity {
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            recyclerView = findViewById(R.id.alarmListRecyclerView);
+            addAlarmFab = findViewById(R.id.floatingActionButton);
+            addAlarmFab.setOnClickListener(v1 -> Toast.makeText(MainActivity.this, "el boton funciona!!!", Toast.LENGTH_SHORT).show());
             return insets;
         });
     }
